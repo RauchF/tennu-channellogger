04cf7d12625dacbbdd21ea2718e1d2331663ef95
Felix Rauch <rauch@skaiamail.de>
2016-03-27 19:49:28 +0200

    Add skeleton files

---
+++ plugin.js
@@ -0,0 +1,20 @@
+var ChannelLogger = {
+	init: function (client, imports) {
+		// Initialization of the plugin
+
+		return {
+			exports: {
+			},
+
+			handlers: {
+			},
+
+			help: {
+			},
+
+			commands: []
+		}
+	}
+}
+
+module.exports = ChannelLogger;
